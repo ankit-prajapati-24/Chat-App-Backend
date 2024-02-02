5c354d2ad430d47dafc703edde113abe8f905818
ankit-prajapati-24 <141832510+ankit-prajapati-24@users.noreply.github.com>
2024-02-02 23:28:47 +0530

    final

--- index.js
+++ index.js
@@ -1,46 +1,54 @@
-const express = require('express');
 const http = require('http');
-const socketIO = require('socket.io');
+const path = require('path');
+const express = require('express');
 const cors = require('cors');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIO(server, {
-  cors:{
-    origin:"*",
-  }
-});
+const io = new Server(server, {
+    cors: {
+      origin: "*",
+      credentials:true,
+      methods: ["GET", "POST"]
+    }
+  });
 
-// Serve your static files or set up your routes
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.resolve('./public')));
 
-// Socket.IO server logic
 io.on('connection', (socket) => {
     console.log('A user connected:', socket.id);
-
-    // Listen for custom 'chat-message' event
-    socket.on('chat-message', (msg) => {
-        console.log('Received message:', msg);
-        // Broadcast the message to all connected clients
-        io.emit('chat-message', msg);
+     
+    socket.on('chat-message', (msg,roomid,) => {
+        if(roomid != ""){
+        const allRooms = io.sockets.adapter.rooms;
+         socket.join(roomid);
+         console.log("all rooms",allRooms,allRooms.has(roomid));
+         socket.to(roomid).emit('chat-message', msg);
+        // socket.broadcast.to(roomid).emit('chat-message', msg);
+        }
+        else{
+            // socket.broadcast.emit('chat-message', msg);
+            socket.join("")
+        }
+        console.log('Received message:', msg,roomid);
     });
 
-    // Disconnect event
     socket.on('disconnect', () => {
         console.log('User disconnected:', socket.id);
     });
 });
 
-// HTTP proxy middleware
-const proxyMiddleware = createProxyMiddleware('/api', {
-    target: 'http://localhost:4000',  // Adjust the backend API server URL
-    changeOrigin: true,
+app.use(
+    cors({
+      origin: "*",
+      credentials: true
+    })
+  );
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.use('/api', proxyMiddleware);
-
-// Start the combined server
 const PORT = process.env.PORT || 4000;
 
 server.listen(PORT, () => {
