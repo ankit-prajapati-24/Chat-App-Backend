95b3514c194f3ab835cfb1bd35f2d547d811d223
ankit-prajapati-24 <141832510+ankit-prajapati-24@users.noreply.github.com>
2024-02-14 13:18:00 +0530

    update image base64

--- utils/imageUploader.js
+++ utils/imageUploader.js
@@ -1,16 +1,22 @@
 const cloudinary = require('cloudinary').v2;
+const fs = require('fs');
 
-exports.uploadImageToCloudinary = async(file,folder,height,quality) =>{
-    
-    const options = {folder};
-    if(height){
-        options.height = height;
-    }
-    if(quality){
-        options.quality = quality;
-    }
-    const base64ImageData = await RNFetchBlob.fs.readFile(imageUri, 'base64');
-    options.resource_type = "auto";
+exports.uploadImageToCloudinary = async (file, folder, height, quality) => {
+    try {
+        const options = { folder };
+        if (height) {
+            options.height = height;
+        }
+        if (quality) {
+            options.quality = quality;
+        }
 
-    return await cloudinary.uploader.upload(base64ImageData,options);
+        const base64ImageData = fs.readFileSync(file, { encoding: 'base64' });
+        options.resource_type = "auto";
+
+        return await cloudinary.uploader.upload(`data:image/png;base64,${base64ImageData}`, options);
+    } catch (error) {
+        console.error("Error uploading image to Cloudinary:", error);
+        throw error;
+    }
 }
