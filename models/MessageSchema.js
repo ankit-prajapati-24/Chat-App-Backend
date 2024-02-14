2c794688e2343ec6884eee09b12145fd3bf59a10
ankit-prajapati-24 <141832510+ankit-prajapati-24@users.noreply.github.com>
2024-02-14 11:57:28 +0530

    update image

--- models/MessageSchema.js
+++ models/MessageSchema.js
@@ -1,16 +1,16 @@
 const mongoose = require("mongoose");
 
-function gettime() {
-    // Function to get current time
-    var currentDate = new Date();
-    var hours = currentDate.getHours();
-    var minutes = currentDate.getMinutes();
-    var meridiem = hours >= 12 ? 'PM' : 'AM';
-    hours = hours % 12;
-    hours = hours ? hours : 12;
-    var currentTime = hours + ':' + (minutes < 10 ? '0' : '') + minutes + ' ' + meridiem;
-    return currentTime;
-  }
+// function gettime() {
+//     // Function to get current time
+//     var currentDate = new Date();
+//     var hours = currentDate.getHours();
+//     var minutes = currentDate.getMinutes();
+//     var meridiem = hours >= 12 ? 'PM' : 'AM';
+//     hours = hours % 12;
+//     hours = hours ? hours : 12;
+//     var currentTime = hours + ':' + (minutes < 10 ? '0' : '') + minutes + ' ' + meridiem;
+//     return currentTime;
+//   }
 
 const MessageSchema = mongoose.Schema({
     Name: {
@@ -25,7 +25,7 @@ const MessageSchema = mongoose.Schema({
     },
     Date: {
         type: String,
-        default: gettime(),
+        default: Date.now(),
     },
     
 });
