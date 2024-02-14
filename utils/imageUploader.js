1545ac8b6f1531fc482f79679925b4ae32e00ec9
ankit-prajapati-24 <141832510+ankit-prajapati-24@users.noreply.github.com>
2024-02-14 13:34:18 +0530

    update image

--- utils/imageUploader.js
+++ utils/imageUploader.js
@@ -1,22 +1,14 @@
 const cloudinary = require('cloudinary').v2;
-const fs = require('fs');
 
-exports.uploadImageToCloudinary = async (file, folder, height, quality) => {
-    try {
-        const options = { folder };
-        if (height) {
-            options.height = height;
-        }
-        if (quality) {
-            options.quality = quality;
-        }
-
-        const base64ImageData = fs.readFileSync(file, { encoding: 'base64' });
-        options.resource_type = "auto";
-
-        return await cloudinary.uploader.upload(`data:image/png;base64,${base64ImageData}`, options);
-    } catch (error) {
-        console.error("Error uploading image to Cloudinary:", error);
-        throw error;
+exports.uploadImageToCloudinary = async(file,folder,height,quality) =>{
+    const options = {folder};
+    if(height){
+        options.height = height;
     }
+    if(quality){
+        options.quality = quality;
+    }
+   options.resource_type = "auto";
+
+    return await cloudinary.uploader.upload(file,options);
 }
